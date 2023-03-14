f20ae5e3d4acf90e21d7749e284e1b25237b117b
Leonardo Py <igusduikana00@gmail.com>
2023-03-14 18:57:34 -0300

    react footer added

---
+++ src/footer.jsx
@@ -0,0 +1,15 @@
+<section className='footer'> 
+
+<img src={logo2} className="logo" alt="logotipo_gimme"/>
+
+  <p> Gimme • All rights reserved.</p>
+
+    <div>
+      <ul  className='list'>
+        <li><a href='#'>Contact</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Links</a></li>
+      </ul>
+   </div>
+
+</section>
