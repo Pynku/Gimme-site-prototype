7d8d4f01ccebe306f6e79206e45860cecbd8608d
Leonardo Py <igusduikana00@gmail.com>
2023-03-14 21:05:26 -0300

    corrected footer

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,15 +1,26 @@
-<section className='footer'> 
+/* isso aqui é a criação de uma função com a variavel chamada footerComponent que o React vai chamar*/
 
-<img src={logo2} className="logo" alt="logotipo_gimme"/>
+function footer () {
 
-  <p> Gimme • All rights reserved.</p>
-
-    <div>
-      <ul  className='list'>
-        <li><a href='#'>Contact</a></li>
-        <li><a href='#'>About</a></li>
-        <li><a href='#'>Links</a></li>
-      </ul>
-   </div>
-
-</section>
+    let footerComponent = (
+      <section className='footer'> 
+    
+    <img src={logo2} className="logo" alt="logotipo_gimme"/>
+    
+      <p> Gimme • All rights reserved.</p>
+    
+        <div>
+          <ul  className='list'>
+            <li><a href='#'>Contact</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Links</a></li>
+          </ul>
+       </div>
+    
+    </section>
+    );
+    
+    return footerComponent;
+    
+    }
+    
